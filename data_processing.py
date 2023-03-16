70fd0c1fc3b65563df93894647682bc766f23f45
Olivier Rogers <orogers@ucsd.edu>
2023-03-15 18:33:10 -0700

    process_yearly & __repr__

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import logging
+from os import path
 from read_data import ReadData
+
 
 logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
@@ -20,6 +22,9 @@ class ProcessData():
         self.salary_functions = [self.__company_comp_salaries]
         logger.debug(f'Initialized ProcessData with data directory: {self.data_directory}')
 
+    def __repr__(self) -> str:
+        return f'ProcessData object\nWorking data directory:\n\t{path.abspath(self.data_directory)}'
+
     def process(self) -> dict:
         """
         Process the data and return a dictionary of processed data
@@ -31,7 +36,55 @@ class ProcessData():
                 self.processed['layoff_processed'] = self.__process_layoffs(self.data[data])
             elif data == 'salaries':
                 self.processed['salary_processed'] = self.__process_salary(self.data[data])
+            elif 'hiring' in data:
+                self.__process_yearly(input_df=self.data[data],
+                                      output_df='hiring',
+                                      df_index='INDUSTRY',)
+            elif 'reason' in data:
+                self.__process_yearly(input_df=self.data[data],
+                                      output_df='reason',
+                                      df_index='reason',)
         return self.processed
+    
+    def __process_yearly(self, input_df: pd.DataFrame, output_df:str, df_index:str) -> None:
+        """
+        Takes Dataframes containing data with numeric columns and adds
+        the data to a specified dataframe.
+        Columns without a numeric label are dropped.
+        Data values should be able to convert into numbers and
+        the entries in the output dataframe will have type int.
+
+        Attributes
+        ----------
+        input_df:
+            input dataframe
+        output_df: str
+            Name of dataframe to add to.
+        df_index: str
+            input dataframe column to be used as index
+        """
+        input_df = input_df.set_index(df_index)
+        for column in input_df.columns:
+            try:
+                input_df = input_df.rename(columns={column:int(column)})
+            except ValueError:
+                logger.debug(f'{column} is not a Year column')
+                input_df = input_df.drop(columns=column)
+        self.__outer_join(input_df=input_df,add_to_df=output_df)
+
+    def __outer_join(self, input_df: pd.DataFrame, add_to_df:str) -> None:
+        """
+        Outer join on index.
+        Data values should be able to convert into numbers and
+        the entries in the output dataframe will have type int.
+        """
+        try:
+            self.processed[add_to_df] = self.processed[add_to_df].join(input_df,how='outer',rsuffix='_dupl').fillna(0)    
+            self.processed[add_to_df] = self.processed[add_to_df].drop(self.processed[add_to_df].filter(regex='_dupl$').columns, axis=1).astype(int)
+            for column in self.processed[add_to_df].columns:
+                self.processed[add_to_df] = self.processed[add_to_df].rename(columns={column:int(column)})
+        except KeyError:
+            self.processed[add_to_df] = input_df
 
     def __process_layoffs(self, data: pd.DataFrame) -> dict:
         """
@@ -95,8 +148,9 @@ class ProcessData():
         return funds[['funds_raised', 'total_laid_off']]
 
 if __name__ == '__main__':
-    data_directory = 'data'
+    # data_directory = 'data'
+    data_directory = 'data/challenger_data'
     data_processor = ProcessData(data_directory)
     data = data_processor.process()
     # print(data)
-    
+    # print(data_processor)
